46c5f0cb78fb5525ab954b1e316a230bd78ac7d2
carocitta <carocitta@gmail.com>
2025-05-09 18:24:49 +0200

    first commit

---
+++ src/main/java/com/will_vila_be/Demo/Back_End/Will/Vila/Project/DemoBackEndWillVilaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.will_vila_be.Demo.Back_End.Will.Vila.Project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBackEndWillVilaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBackEndWillVilaProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/will_vila_be/Demo/Back_End/Will/Vila/Project/DemoBackEndWillVilaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.will_vila_be.Demo.Back_End.Will.Vila.Project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBackEndWillVilaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
